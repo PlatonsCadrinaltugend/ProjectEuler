4256042cd899140a44d804c01eca2ea608442d6c
Dominik Fritsche <Dominik.Fritsche@outlook.de>
2023-05-16 14:57:25 +0200

    euler014

---
+++ euler014/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let mut max = 0;
+    let mut xx = 0;
+    for x in 1..=1_000_000{
+        let new = collatz(x);
+        if new>max{
+            max = new;
+            xx = x;
+        }
+    }
+    println!("{}", xx);
+}
+
+fn collatz(mut number:u64) -> u64{
+    let mut terms = 1;
+    while number != 1{
+        if number%2 == 0{
+            number /=2;
+        }
+        else{
+            number = number*3 +1;
+        }
+        terms +=1;
+    }
+    terms
+}
