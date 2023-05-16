00ba4c2c1016dd1f99ca211e7a843954fe87fa1e
Dominik Fritsche <dominik.fritsche@mailbox.tu-dresden.de>
2023-05-16 18:49:13 +0200

    euler019

---
+++ euler019/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    /* 
+    1.1.1901 Dienstag
+    99 Jahre
+    1904 1. Schaltjahr
+    */
+    let mut sundays = 0;
+    let mut current_monthstart = 2;
+    for x in 1901..=2000{
+        for y in 1..=12{ 
+            if y==9 || y==4 || y==6 || y==11 {
+                current_monthstart = (current_monthstart + 2)%7;
+            }
+            else if y==2{
+                if x%4 == 0 && x%100!=0 || x%400 == 0{
+
+                    current_monthstart = (current_monthstart + 1)%7;
+                }
+            }
+            else{
+                current_monthstart = (current_monthstart +3)%7;
+            }
+            if current_monthstart == 0{
+                sundays+=1;
+            }
+        }
+    }
+    println!("{}", sundays);
+}
