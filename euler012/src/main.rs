01d5b9540c4ad99df9960d432f77a0ac4671dbf0
Dominik Fritsche <Dominik.Fritsche@outlook.de>
2023-05-16 14:28:16 +0200

    euler012

---
+++ euler012/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let mut current = 1;
+    let mut last = 1;
+    let dividentse = 500;
+    loop{
+        if dividents(last as f64) > dividentse{
+            break
+        }
+        current +=1;
+        last += current;
+    }
+    println!("{}", last);
+}
+
+fn dividents(number:f64)-> i64{
+    let mut amount = 0;
+    for x in 1..=(number.sqrt() as i64){
+        if (number as i64)%x==0{
+            if x == (number.sqrt() as i64){
+                amount +=1;
+            }
+            else{
+                amount+=2;
+            }
+        }
+    }
+    amount
+}
