3fd60189d352ce8f972a131c1c57cb8842588e64
Dominik Fritsche <dominik.fritsche@mailbox.tu-dresden.de>
2023-05-16 21:02:40 +0200

    euler021

---
+++ euler021/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let mut sum = 0;
+    let mut vec = Vec::new();
+    vec.push(0);
+    for a in 1..=10_000{
+        vec.push(d(a));
+    }
+    for x in 0..10_000{
+        if vec.contains(&vec[x]){
+            let pos = vec.iter().position(|&r| r == vec[x]).unwrap();
+            if vec.contains(&vec[pos]) && vec[x] != x.try_into().unwrap(){
+                if d(x as u128) == vec[pos] && x == d(vec[pos]).try_into().unwrap(){
+                    sum +=vec[x];
+                    println!("{}", x);
+                }
+            }
+        }
+    }
+    println!("{}", sum);
+}
+fn d(n: u128)-> u128{
+    let mut sum = 0;
+    for x in 1..=(n/2){
+        if n%x == 0{
+            sum += x;
+        }
+    }
+    sum
+}
