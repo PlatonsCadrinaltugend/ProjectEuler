3fb70f01bc5e88f2a10ff772712cdfaca75a76c1
Dominik Fritsche <Dominik.Fritsche@outlook.de>
2023-05-16 15:30:30 +0200

    euler015

---
+++ euler015/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let mut array : [Vec<i64>; 21] = core::array::from_fn(|_| Vec::new());
+    for y in 0..=20{
+        let vecotr: Vec<i64> = Vec::new();
+        array[y]=vecotr;
+    }
+    for z in 0..=20{
+        for _a in 0..=20{
+            array[z].push(0);
+        }
+        
+    }
+    for x in 0..=20{
+        for y in 0..=20{
+            if x>0{
+                if y>0{
+                    array[x][y] = array[x-1][y] + array[x][y-1];
+                }
+                else{
+                    array[x][y] = 1;
+                }
+            }
+            else{
+                array[x][y] = 1;
+            }
+        }
+        println!();
+    }
+    println!("{}", array[20][20]);
+}
