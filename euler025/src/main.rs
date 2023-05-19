7528ef0d0cd2ee95e00ea3f87a0b82809a4487c9
Dominik Fritsche <dominik.fritsche@mailbox.tu-dresden.de>
2023-05-19 17:04:15 +0200

    euler025

---
+++ euler025/src/main.rs
@@ -0,0 +1,15 @@
+use num_bigint::BigUint;
+fn main() 
+    let mut current = BigUint::from(1 as u64);
+    let mut before = Vec::new();
+    before.push(BigUint::from(1 as u64));
+    before.push(BigUint::from(1 as u64));
+    let mut pos = 2;
+    let power = BigUint::from(10 as u64).pow(999); // digits -1 
+    while current.clone()/power.clone()< BigUint::from(1 as u64){
+        current = &before[pos-1] + &before[pos-2];
+        before.push(current.clone());
+        pos+=1;
+    }
+    println!("{}", pos);
+
