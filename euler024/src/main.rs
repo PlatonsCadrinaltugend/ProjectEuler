a89ce9d78ac8316d52f5370912f90bd95329fa42
Dominik Fritsche <dominik.fritsche@mailbox.tu-dresden.de>
2023-05-19 16:36:43 +0200

    euler024, rework could be good

---
+++ euler024/src/main.rs
@@ -0,0 +1,8 @@
+use itertools::Itertools;
+
+fn main() {
+    let permutations = vec![0,1,2,3,4,5,6,7,8,9];
+    if let Some(solution) =permutations.iter().permutations(permutations.len()).nth(999_999){
+        println!("{:?}", solution);
+}
+}
